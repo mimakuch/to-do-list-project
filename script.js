bba4319e5bedf9c4e6f9b86caa4ebebc8d3fa612
Michal Makuch <michalmakuch@hotmail.com>
2022-08-28 17:18:08 +0200

    addTodo function added

---
+++ script.js
@@ -0,0 +1,36 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-btn');
+const todoList = document.querySelector('.todo-list');
+
+
+// Event listeners
+todoButton.addEventListener('click', addTodo);
+
+
+// Functions
+
+function addTodo(event) {
+    // Prevent form from submitting
+    event.preventDefault();
+    // Todo DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    // Create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = "hey";
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    // Check Mark Button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+    // Delete Button
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+    deleteButton.classList.add('delete-btn');
+    todoDiv.appendChild(deleteButton);
+    // Append list
+    todoList.appendChild(todoDiv);
+}
